d689dbdb8bedc3a308b82cabd39decdb58d51940
MeghaSinghall <meghasinghal32357@gmail.com>
2024-10-13 22:01:59 +0530

    update

--- script.js
+++ script.js
@@ -6,18 +6,10 @@ var1.addEventListener("keyup",e=>{
     var1.style.height=`${height1}px`;
     var2.style.height=`${height1+20}px`;
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 8eaddbab88a28f28e2aa516924eee5e61e348007
 document.getElementById("chatbtn").onclick=function(){
     let input1=document.getElementById("chattext").value;
     console.log(input1);
 }
 document.getElementById("chatrfs").onclick=function(){
     document.getElementById("chattext").value="";
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 8eaddbab88a28f28e2aa516924eee5e61e348007
