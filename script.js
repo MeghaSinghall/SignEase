9ae486eb0f12c5e73734258d4c6747e1459b18da
MeghaSinghall <meghasinghal32357@gmail.com>
2024-10-16 01:20:53 +0530

    up

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var vdobox=document.getElementById("bodyvdo");
-var d1=0;
+const input1=document.getElementById("chattext");
 
 const var1=document.querySelector("#chattext");
 const var2=document.querySelector("#chatdiv");
@@ -10,9 +9,10 @@ var1.addEventListener("keyup",e=>{
     var2.style.height=`${height1+20}px`;
 });
 
-document.getElementById("chatbtn").onclick=function(){
-    let input1=document.getElementById("chattext").value;
-    console.log(input1);
+var vdobox=document.getElementById("bodyvdo");
+var d1=0;
+function btnclick(){
+    console.log(input1.value);
     
     // if(d1==1)
     // {
@@ -24,9 +24,23 @@ document.getElementById("chatbtn").onclick=function(){
         d1=1;
     }
 }
-document.getElementById("chatrfs").onclick=function(){
-    document.getElementById("chattext").value="";
 
+var1.addEventListener("keyup",e=>{
+    e.preventDefault();
+    if(e.keyCode===16){
+        var1.addEventListener("keyup",e=>{
+            e.preventDefault();
+            if(e.keyCode===13){
+                btnclick();
+            }
+        })
+    }
+})
+
+document.getElementById("chatrfs").onclick=function(){
+    input1.value="";
+    var1.style.height=`${25}px`;
+    var2.style.height=`${45}px`;
     if(d1==1)
         {
             vdobox.style.display='none';
